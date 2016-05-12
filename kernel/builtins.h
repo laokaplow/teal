2ada5825f589c9e1934caaa0d4c06116272b8b89
Lao Akili <lao@laokaplow.com>
2016-05-12 15:21:36 -0700

    clean up repo

--- kernel/builtins.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUILTINS_H_
-#define BUILTINS_H_
-
-#include "memory.h"
-#include "types.h"
-
-Ref<List> default_env();
-
-#endif
--- kernel/special_forms.h
+++
@@ -1,6 +0,0 @@
-#define SPECIAL_FORMS_H_
-#define SPECIAL_FORMS_H_
-
-void define_special_forms(Ref<List> env);
-
-#endif
